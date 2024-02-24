7ffa5a2142d244984b9e5888bbf2693dff312c7a
Engr. Aldren Letada <60842892+GarudaKurt@users.noreply.github.com>
2024-02-24 21:59:16 +0800

    Finally Done ready for testing and deployment

---
+++ components/Researve.js
@@ -0,0 +1,223 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Modal, TextInput,Pressable } from 'react-native';
+import { Card, Title, Paragraph, Button } from 'react-native-paper';
+import { db } from './firebaseConfig';
+import { collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
+
+const EventCard = ({ event, onDelete, showModal }) => (
+  <View style={[styles.cardContainer, styles.card]}>
+    <Card>
+      <Card.Content>
+        <Title>{event.subjectcode}</Title>
+            <Paragraph>Prof ID:{event.userid}</Paragraph>
+            <Paragraph>Item: {event.tv}</Paragraph>
+            <Paragraph>Item: {event.hdmi}</Paragraph>
+            <Paragraph>Item: {event.projector}</Paragraph>
+            <Paragraph>Sched: {event.schedule}</Paragraph>
+      </Card.Content>
+      <Card.Actions>
+        <TouchableOpacity onPress={() => showModal(event)}>
+          <Text style={styles.buttonText}>Remove</Text>
+        </TouchableOpacity>
+      </Card.Actions>
+    </Card>
+  </View>
+);
+
+const Researve = ({navigation}) => {
+  const [events, setEvents] = useState([]);
+  const [removeModalVisible, setRemoveModalVisible] = useState(false);
+  const [removeUserID, setRemoveUserID] = useState('');
+  const [eventToRemove, setEventToRemove] = useState(null);
+  const [userID, setUserID] = useState(''); // Initialize userID state variable
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
+ 
+  const dashboard = () => {
+    navigation.replace('Admin')
+  }
+
+  useEffect(() => {
+    updateDb();
+    // Set userID when removeUserID changes
+    setUserID(removeUserID);
+  }, [removeUserID]); // Run the effect whenever removeUserID changes
+
+  const updateDb = async () => {
+    try {
+      const userSnapshot = await getDocs(collection(db, "users"));
+      const fetchedEvents = [];
+
+      userSnapshot.forEach((doc) => {
+        const userData = doc.data();
+        if (userData.Code && userData.TV && userData.HDMI && userData.Projector && userData.userID, userData.Schedule) { // Check if Title and Description exist
+            fetchedEvents.push({
+              subjectcode: userData.Code,
+              hdmi: userData.HDMI,
+              projector: userData.Projector,
+              tv: userData.TV,
+              userid: userData.userID,
+              schedule: userData.Schedule
+            });
+        }
+      });
+
+      setEvents(fetchedEvents);
+    } catch (error) {
+      console.log("Error fetching events: ", error);
+    }
+  };
+
+  const handleDeleteEvent = async () => {
+    try {
+      if (!eventToRemove) return;
+
+      await deleteDoc(doc(db, "users", eventToRemove.id));
+      setEvents(events.filter(event => event.id !== eventToRemove.id));
+      setEventToRemove(null);
+      setRemoveModalVisible(false);
+    } catch (error) {
+      console.log("Error deleting event: ", error);
+    }
+  };
+
+  const handleRemoveEventSubmit = async () => {
+    try {
+      if (removeUserID === userID) {
+        const updatedEvents = events.filter(event => event.id !== eventToRemove.id);
+        setEvents(updatedEvents);
+
+        // Check if the user is an instructor
+        const userSnapshot = await getDocs(collection(db, "users"));
+        let userExists = false;
+
+        userSnapshot.forEach((doc) => {
+          const userData = doc.data();
+          if (parseInt(userData.userID) === parseInt(userID) && userData.Type === "Instructor") {
+            userExists = true;
+            // Delete Title and Description fields
+            updateDoc(doc.ref, {
+              Title: '',
+              Description: '',
+            });
+          }
+        });
+
+        if (!userExists) {
+          setModalMessage("Access denied: only instructor can remove.");
+          setModalVisible(true);
+          return;
+        }
+        setRemoveModalVisible(false);
+      } else {
+        setModalMessage("Access denied: only instructor can remove.");
+        setModalVisible(true);
+      }
+    } catch (error) {
+      console.log("Error removing event: ", error);
+    }
+  };
+
+  const showModal = (event) => {
+    setEventToRemove(event);
+    setRemoveModalVisible(true);
+  };
+
+  return (
+    <View style={styles.formContainer}>
+      <Text style={styles.title}> Admin Dashboard</Text>
+      <Pressable onPress={dashboard}>
+        <Text>→ Dashboard</Text>
+      </Pressable>
+      <ScrollView contentContainerStyle={styles.scrollContainer} horizontal={true}>
+        {events.map((event, index) => (
+          <EventCard key={index} event={event} onDelete={handleDeleteEvent} showModal={showModal} />
+        ))}
+      </ScrollView>
+
+      <Modal
+        visible={removeModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setRemoveModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your instructor id"
+            value={removeUserID}
+            onChangeText={setRemoveUserID}
+          />
+          <Button onPress={handleRemoveEventSubmit} >Submit </Button>
+          <Button onPress={() => setRemoveModalVisible(false)} >Cancel</Button>
+        </View>
+      </Modal>
+
+      {/* Modal for displaying messages */}
+      <Modal
+        visible={modalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <Text>{modalMessage}</Text>
+          <Button onPress={() => setModalVisible(false)} title="Close" />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+export default Researve;
+
+const styles = StyleSheet.create({
+  formContainer: {
+    backgroundColor: '#f5f6fa',
+    borderRadius: 10,
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  cardContainer: {
+    marginBottom: 10,
+    marginRight: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  card: {
+    width: 200,
+  },
+  buttonText: {
+    color: 'red',
+  },
+  buttonModal: {
+    backgroundColor: 'black',
+    borderRadius: 5,
+    padding: 3,
+    color: 'white'
+  },
+  buttonCancel: {
+    backgroundColor: 'transparent',
+    color: 'white',
+    borderRadius: 3,
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderRadius: 10,
+    alignItems: 'center',
+    margin: 50,
+  },
+  textInput: {
+    height: 40,
+    width: '15%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+    marginTop: 20,
+  },
+});
